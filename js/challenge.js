264796723b68bc387b7ff4d46e68b954c9166662
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-22 16:10:25 +1000

    Decrement added

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function(){
+//Interval counter to start when clicked and reset when clicked again
 
 let display = document.getElementById('counter')
-
 let clicked = 'false'
 let count = 0
 
@@ -17,9 +17,21 @@ function(){
         clearInterval(ex)
         count = 0
         display.innerHTML = count
-        clicked = 'false'}})
+        clicked = 'false'
+    }})
 
+//
 
+let minus = document.getElementById('minus')
+let plus = document.getElementById('plus')
+let heart = document.getElementById('heart')
+let pause = document.getElementById('pause')
 
+//Decrement counter
+minus.addEventListener('click', function(){
+    count = count - 1
+    display.innerHTML = count
+    console.log(count)
+})
 
-
+})
